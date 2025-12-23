627695ef8567eb6d28eb3caedbbc56b09db36d14
dju78 <1978dju@gmail.com>
2025-12-23 23:19:26 +0000

    fix: switch to HashRouter for GH Pages and support secrets in CI

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
@@ -9,9 +9,9 @@ import ErrorBoundary from './components/ErrorBoundary.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ErrorBoundary>
-            <BrowserRouter basename="/kogiqest/">
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </ErrorBoundary>
     </React.StrictMode>,
 )
